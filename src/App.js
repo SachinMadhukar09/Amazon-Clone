bb7bec45c15e990e8a7219d968953055bddc44a3
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-08-16 23:13:44 +0530

    Home Page Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header/>
-      {/* Home */}
-
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
