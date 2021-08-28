3e3cfba13dcb69809c096dc1a096fc0749a9a021
Sachin Madhukar <sachinkumarmadhukar@gmail.com>
2021-08-28 20:30:46 +0530

    Authentication Begin

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React , {useEffect} from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -5,20 +6,46 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login"
+import { auth } from './firebase';
+import { ConsoleWriter } from 'istanbul-lib-report';
 
 function App() {
+
+  useEffect(() => {
+    // will only runs once when the app component loads..
+
+      auth.onAuthStateChanged(authUser => {
+        ConsoleWriter.log("THE USER IS >>> " , authUser)
+
+        if (authUser){
+          // the user just logged in / the user was logged in
+          dispatchEvent({
+            type:"SET_USER",
+            user:authUser
+          })
+        }else {
+          // the user is logged out
+          dispatchEvent({
+            type:"SET_USER",
+            user:null
+          })
+        }
+      })
+  }, [])
   return (
     <Router>
       <div className="App">
-        <Header />
         <Switch>
-        <Route path="/login">
-            <h2>Login Page</h2>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+          <Header />
             <Home />
           </Route>
         </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+// import * as firebase from 'firebase'
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDyRrojB-fhbB5Y7au8uonh3b7P4ZT_Hjo",
-    authDomain: "clone-aadf7.firebaseapp.com",
-    projectId: "clone-aadf7",
-    storageBucket: "clone-aadf7.appspot.com",
-    messagingSenderId: "756007236837",
-    appId: "1:756007236837:web:9548cda1439459c8d8900c",
-    measurementId: "G-WRCDTZ1WY5"
-  };
+  apiKey: "AIzaSyDyRrojB-fhbB5Y7au8uonh3b7P4ZT_Hjo",
+  authDomain: "clone-aadf7.firebaseapp.com",
+  projectId: "clone-aadf7",
+  storageBucket: "clone-aadf7.appspot.com",
+  messagingSenderId: "756007236837",
+  appId: "1:756007236837:web:9548cda1439459c8d8900c",
+  measurementId: "G-WRCDTZ1WY5",
+};
+
+const firebaseApp = firebase.intializeApp({firebaseConfig});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
